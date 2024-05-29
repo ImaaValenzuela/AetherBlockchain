2ffd469ffae92c9151db428bbfa42f443909b4cb
Mona Lisa <imanolvalenzuela03@gmail.com>
2024-05-29 17:39:48 -0300

    Implements P2P

--- src/service/index.js
+++ src/service/index.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import Blockchain from '../blockchain';
 
-const {HTTP_PORT = 3000} = process.env;
+import Blockchain from '../blockchain';
+import P2PService from './p2p';
+
+const { HTTP_PORT = 3000 } = process.env;
 
 const app = express();
 const blockchain = new Blockchain();
-
-blockchain.addBlock('express');
+const p2pService = new P2PService(blockchain);
 
 app.use(bodyParser.json());
 
@@ -15,16 +16,19 @@ app.get('/blocks', (req, res) => {
     res.json(blockchain.blocks);
 });
 
-app.post('/mine', (req, res) =>{
-    const {body: {data}} = req;
+app.post('/mine', (req, res) => {
+    const { body: { data } } = req;
     const block = blockchain.addBlock(data);
+
+    p2pService.sync();
 
     res.json({
         blocks: blockchain.blocks.length,
         block,
-    })
+    });
 });
 
 app.listen(HTTP_PORT, () => {
-    console.log(`Service HTTP:${HTTP_PORT} listening . . .`);
+    console.log(`Service HTTP:${HTTP_PORT} listening...`);
+    p2pService.listen();
 });
